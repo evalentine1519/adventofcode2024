308e40334fda37e85b301955d28bd9145b73298f
evalentine1519 <16657696+evalentine1519@users.noreply.github.com>
2024-12-04 10:09:29 -0800

    Finished part 2 of day 4

--- day4.py
+++ day4.py
@@ -2,32 +2,34 @@ f = open('day4input.txt', 'r')
 data = f.read().splitlines()
 
 movements = [(1,0),(-1,0),(0,1),(0,-1),(-1,-1),(-1,1),(1,-1),(1,1)]
-count = 0
+diagmovements = [[(-1,1),(1,-1)],[(-1,-1),(1,1)]]
+xmascount = 0
+mascount = 0
 
+#part 1
 #search the rows
 for i, row in enumerate(data):
     #search the letters in the rows
     for j, char in enumerate(row):
         if char == 'X':
-            #print("X found at: " + str(i) + ',' + str(j))
             #if 'X' found, start searching each direction
             for move in movements:
                 newi = i + move[0]
                 newj = j + move[1]
                 #if moving won't take us out of bounds and letter 2 is M
                 if 0 <= newi < 140 and 0 <= newj < 140 and data[newi][newj] == 'M':
-                    #print("M found at: " + str(newi) + ',' + str(newj))
+
                     newi += move[0]
                     newj += move[1]
                     #if moving won't take us out of bounds and letter 3 is A
                     if 0 <= newi < 140 and 0 <= newj < 140 and data[newi][newj] == 'A':
-                        #print("A found at: " + str(newi) + ',' + str(newj))
+
                         newi += move[0]
                         newj += move[1]
                         #if moving won't take us out of bounds and letter 4 is S
                         if 0 <= newi < 140 and 0 <= newj < 140 and data[newi][newj] == 'S':
-                            #print("S found at: " + str(newi) + ',' + str(newj))
-                            count += 1
+
+                            xmascount += 1
                         else:
                             continue
                     else:
@@ -35,4 +37,43 @@ for i, row in enumerate(data):
                 else:
                     continue
 
-print(count)
+print(xmascount)
+
+#part 2
+for i, row in enumerate(data):
+    for j, char in enumerate(row):
+        chars = []
+        if char == 'A':
+            for move in diagmovements:
+                newi = i + move[0][0]
+                newj = j + move[0][1]
+                newii = i + move[1][0]
+                newjj = j + move[1][1]
+
+                inboundij = False
+                inboundiijj = False
+                if 0 <= newi < 140 and 0 <= newj < 140:
+                    inboundij = True
+                
+                if 0 <= newii < 140 and 0 <= newjj < 140:
+                    inboundiijj = True
+
+                if inboundij and data[newi][newj] == 'M':
+                    if inboundiijj and data[newii][newjj] == 'S':
+                        chars.append(data[newi][newj])
+                        chars.append(data[newii][newjj])
+                    else:
+                        break
+                elif inboundij and data[newi][newj] == 'S':
+                    if inboundiijj and data[newii][newjj] == 'M':
+                        chars.append(data[newi][newj])
+                        chars.append(data[newii][newjj])
+                    else:
+                        break
+                else:
+                    break
+
+            if len(chars) == 4 and chars.count('S') == 2 and chars.count('M') == 2:
+                mascount += 1
+
+print(mascount)
